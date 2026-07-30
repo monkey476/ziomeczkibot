d70975e8cc4dcfa6dca2af7d382833fa49c0fc27
monkey476 <stastomczuk@gmail.com>
2026-07-31 01:07:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+// Prosty serwer HTTP, żeby Render nie uśpił bota
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot Ziomeczki.gg działa!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer HTTP uruchomiony na porcie ${PORT}`);
+});
+
+// Kod bota Discord
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('ready', () => {
