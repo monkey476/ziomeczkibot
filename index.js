d5e30d15bc8ec8fd5717e841e18a1e9fb497e220
monkey476 <stastomczuk@gmail.com>
2026-08-01 15:06:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
     ]
 });
 
-// --- PROSTY SERWER WWW DLA HOSTINGU (Zapewnia, że bot jest cały czas aktywny) ---
+// --- PROSTY SERWER WWW DLA HOSTINGU (Zapewnia, że bot jest cały czas aktywny 24/7) ---
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Bot Ziomeczki.gg dziala 24/7 i jest aktywny!\n');
@@ -21,7 +21,7 @@ server.listen(3000, () => {
     console.log('🌐 Serwer WWW dla utrzymania aktywności ruszył na porcie 3000.');
 });
 
-// --- URUCHOMIENIE BOTA I MODUŁÓW ---
+// --- URUCHOMIENIE BOTA I WSZYSTKICH MODUŁÓW ---
 client.once('ready', () => {
     console.log(`✅ Zalogowano pomyślnie jako: ${client.user.tag}`);
     
@@ -31,6 +31,7 @@ client.once('ready', () => {
     require('./weryfikacja.js')(client);
     require('./tickety.js')(client);
     require('./liczenie.js')(client);
+    require('./ostatnia-litera.js')(client);
 });
 
 // Logowanie za pomocą zmiennej środowiskowej DISCORD_TOKEN
